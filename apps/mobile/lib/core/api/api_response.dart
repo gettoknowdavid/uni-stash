2eed66366704cc3f033622ae5d6562891f0c61d8
David Michael II <knowdavidmichael@gmail.com>
2026-08-26 22:48:14 +0100

    feat: Add generic ApiResponse model

---
+++ apps/mobile/lib/core/api/api_response.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'api_response.freezed.dart';
+part 'api_response.g.dart';
+
+@Freezed(genericArgumentFactories: true)
+abstract class ApiResponse<T> with _$ApiResponse<T> {
+  const factory ApiResponse({
+    required bool success,
+    required String message,
+    T? data,
+  }) = _ApiResponse<T>;
+
+  factory ApiResponse.fromJson(
+    Map<String, dynamic> json,
+    T Function(Object?) fromJsonT,
+  ) => _$ApiResponseFromJson(json, fromJsonT);
+}
