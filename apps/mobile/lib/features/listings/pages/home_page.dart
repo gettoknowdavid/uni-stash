6072965ed2e3d4f79e1d130cd43725bac293713d
David Michael II <knowdavidmichael@gmail.com>
2026-08-29 20:42:45 +0100

    feat: Add logout button to home page

--- apps/mobile/lib/features/listings/pages/home_page.dart
+++ apps/mobile/lib/features/listings/pages/home_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/widgets.dart';
 import 'package:forui/forui.dart';
+import 'package:uni_stash_mobile/core/config/di.dart';
+import 'package:uni_stash_mobile/features/auth/view_models/auth_view_model.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const FScaffold(
-      header: FHeader(title: Text('Home')),
-      child: Text('Home'),
+    return FScaffold(
+      header: const FHeader(title: Text('Home')),
+      child: Center(
+        child: FButton(
+          onPress: () => di<AuthViewModel>().unauthenticate(),
+          child: const Text('Logout'),
+        ),
+      ),
     );
   }
 }
