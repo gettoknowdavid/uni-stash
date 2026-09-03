890db931089cd3f806e3159b04e42e61c733d2ef
David Michael II <knowdavidmichael@gmail.com>
2026-09-03 20:11:40 +0100

    feat: Add forgot password page and auth exports

---
+++ apps/mobile/lib/features/auth/pages/forgot_password_page.dart
@@ -0,0 +1,10 @@
+import 'package:material_ui/material_ui.dart';
+
+class ForgotPasswordPage extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
