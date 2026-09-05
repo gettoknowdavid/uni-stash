52fd4395084b148ccf3d9f1d120f3585e25eb95f
David Michael II <knowdavidmichael@gmail.com>
2026-09-05 01:09:15 +0100

    refactor(mobile): Update HomePage and UsPageHeader widgets

--- apps/mobile/lib/features/listings/pages/home_page.dart
+++ apps/mobile/lib/features/listings/pages/home_page.dart
@@ -2,15 +2,18 @@ import 'package:flutter/widgets.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:uni_stash_mobile/core/config/di.dart';
 import 'package:uni_stash_mobile/features/auth/view_models/auth_view_model.dart';
+import 'package:uni_stash_mobile/shared/widgets/_widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const .symmetric(horizontal: 24),
-      child: Center(
+    return UsPage(
+      header: UsPageHeader(
+        title: Text('UNI·STASH'),
+      ),
+      body: Center(
         child: ShadButton.outline(
           onPressed: () => di<AuthViewModel>().unauthenticate(),
           child: const Text('LOGOUT'),
